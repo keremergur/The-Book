547324143cb68c84f29759ab52a391e094246316
Kerem Ergur <ergur.kerem@gmail.com>
2022-03-20 21:55:10 +0300

    first changes

---
+++ Java/1.2 Syntax.java
@@ -0,0 +1,24 @@
+package Java; //name of the above directory
+
+ class Syntax {
+     /* all java code is written inside a class
+        (other options also available, will be discussed later) */
+
+        public static void main(String[] args){
+            /*
+            method syntax:
+            (<modifiers>) <return type> <name> <arguments>
+            body: {...}
+            */
+
+            String name = "Kerem";          // " for strings
+            char init = 'K';            // ' for characters          
+            final double PI = 3.14; // numbers written as is
+            /*
+            variable syntax:
+            (<modifiers>) <type> <name> (<initialization>)
+            all statements end with a semicolon (;)
+            */
+
+        }
+ }

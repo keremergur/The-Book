5919c6acd879261693b8c5e2d2f1406a4a17fd8f
Kerem Ergür <ergur.kerem@gmail.com>
2023-03-12 22:06:30 +0300

    c loops

---
+++ C/Loops.c
@@ -0,0 +1,23 @@
+
+int main(){
+
+    int i = 0;
+
+    while(i < 10){
+        i++;
+    }
+
+    i = 0;
+
+    do {
+        i++;
+    } while(i < 11);
+
+    for(int i = 0; i < 10; i++){
+        // loop
+    }
+
+    //break, continue
+
+    return 0;
+}

28d3914d960200153bde4cd7e2015f38842babb9
Kerem Ergür <ergur.kerem@gmail.com>
2023-03-24 20:48:31 +0300

    js import

---
+++ Javascript/Imports.js
@@ -0,0 +1,9 @@
+
+// const myvar = require('./Exports')
+
+import {fileVar1, fileVar2} from './Exports'
+
+import {fV7 as fileVar7, fV8 as fileVar8} from './Exports'
+
+import varname, {fileVar9, fileVar10} from './Exports'
+// varname will import default export

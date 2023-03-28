6360017fe287c9335d3c06d48624c01660ea3f1c
Kerem Ergür <ergur.kerem@gmail.com>
2023-03-28 23:17:29 +0300

    java sync

---
+++ Java/Synchronized.java
@@ -0,0 +1,43 @@
+
+class Sync {
+
+    public Sync attr1;
+    public static Sync attr2;
+
+    public synchronized void func1() {
+        // locks function
+    }
+
+    public void func2() {
+        synchronized(this) {
+            // locks calling object
+        }
+    }
+
+    public static void func3() {
+        synchronized(Sync.class) {
+            // locks class
+        }
+    }
+
+    public void func4() {
+        synchronized(attr1) {
+            // locks instance variable
+        }
+        synchronized(attr2) {
+            // locks class variable
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        Sync s = new Sync();
+
+        synchronized(s) {
+            // locks specified object
+            synchronized(s) {
+                // nested allowed
+            }
+        }
+    }
+}
